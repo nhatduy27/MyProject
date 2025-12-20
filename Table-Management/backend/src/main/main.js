cf667ce33fb2da3b125ac0e68b18bf2e79bf791c
Duy Nguyễn <nhatduy123n@gmail.com>
2025-12-20 20:58:18 +0700

    update

--- Table-Management/backend/src/main/main.js
+++ Table-Management/backend/src/main/main.js
@@ -10,15 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000; 
 
 // Middleware
-app.use(cors({
-  origin: [
-    'https://tablemanagementfrontend.onrender.com',
-    'http://localhost:5173'                          
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
--- Table-Management/frontend/vite.config.js
+++ Table-Management/frontend/vite.config.js
@@ -4,5 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  server: {
+    port: 5174,                    // Đổi port local thành 3000
+    host: 'localhost',             // Hoặc '0.0.0.0' để truy cập từ thiết bị khác
+    open: true                     // Tự động mở browser
+  },
+
   plugins: [react(), tailwindcss()],
 })
